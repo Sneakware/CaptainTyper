28b3b48ac0a01a55dea64fac1409c37922935164
Sneakware <s@furtive.link>
2015-05-25 19:53:53 +0200

    chore(core): add basic files

---
+++ server.js
@@ -0,0 +1,19 @@
+var port = 9000;
+
+var webpack          = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config           = require('./webpack.config');
+
+var webPackOptions = {
+  publicPath         : config.output.publicPath,
+  stats              : { colors: true },
+  historyApiFallback : true
+};
+
+var server = new WebpackDevServer(webpack(config), webPackOptions);
+
+server
+  .listen(port, 'localhost', function (err) {
+    if (err) { return console.log(err); }
+    console.log('Listening at localhost:' + port);
+  });
