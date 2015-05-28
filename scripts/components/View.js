e9b2e1146a205551b7a63de5795e86d67559458c
Sneakware <s@furtive.link>
2015-05-28 02:00:02 +0200

    style(view): basic layout

--- scripts/components/View.js
+++ scripts/components/View.js
@@ -8,10 +8,17 @@ module.exports = class View extends React.Component {
     this.ctx = this.canvas.getContext('2d');
 
     this.canvas.width = window.innerWidth;
-    this.canvas.height = 400;
+    this.canvas.height = 600;
 
     this.ctx.fillStyle = '#268bd2';
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    var water = this.canvas.height / 2 + 50;
+    this.ctx.fillStyle = '#073642';
+    this.ctx.fillRect(0, water, this.canvas.width, this.canvas.height / 2);
+
+    this.ctx.fillStyle = '#2aa198';
+    this.ctx.fillRect(0, water, this.canvas.width, 5);
 
   }
 
